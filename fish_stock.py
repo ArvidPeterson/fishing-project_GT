43f37d3935cf4109fee92152077ad59082e6e8a5
Linn <lyster@student.chalmers.se>
2019-12-09 14:21:53 +0100

    Changed som parameters

--- fish_stock.py
+++ fish_stock.py
@@ -2,23 +2,23 @@ class FishStock:
 
     def __init__(self):
 
-        self.carrying_cap = 1
-        self.growth_rate = 1
+        # Todo: Set reasonable parameter values
+        self.carrying_cap = 100
+        self.growth_rate = 10
         self.catch_coeff = 1
-        self.X = 0
+        self.X = 10
 
     def fish_stock_change(self, list_of_fishers):
-
         sum_of_harvest = 0
-        for i_fisher in list_of_fishers:
+        for i_fisher in list_of_fishers:  # Update harvest for every fisher
             i_fisher.harvest = self.catch_coeff * i_fisher.effort * self.X
             sum_of_harvest += i_fisher.harvest
 
-        dXdt = self.logistic_growth() - sum_of_harvest
+        stock_change = self.logistic_growth() - sum_of_harvest
+        self.X += stock_change
 
-        self.X += dXdt
-
+        if self.X < 0:  # Fish stock cannot be negative
+            self.X = 0
 
     def logistic_growth(self):
-
         return self.growth_rate*self.X*(1 - self.X / self.carrying_cap)
--- simulation_of_fish.py
+++ simulation_of_fish.py
@@ -1,21 +1,56 @@
 from fisherman import *
 from fish_stock import *
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def stable_solution(list_of_fishers, fish_stock):
+
+    stable_effort_all_players = []
+    for i, fisher in enumerate(list_of_fishers):
+        if i == len(list_of_fishers)-1:
+            effort_of_other_player = list_of_fishers[0].effort
+        else:
+            effort_of_other_player = list_of_fishers[i+1].effort
+
+        b = fisher.cost_per_effort/(fisher.price_per_harvest*fish_stock.catch_coeff*fish_stock.carrying_cap)
+        stable_effort = (fish_stock.growth_rate/(2*fish_stock.catch_coeff))*(1-b)-0.5*effort_of_other_player
+
+        stable_effort_all_players.append(stable_effort)
+
+    return stable_effort_all_players
+
 
 def main():
+    nbr_players = 2
+    max_time = 10
 
-    nbrPlayers = 2
     list_of_fishers = []
-    for i in range(nbrPlayers):
+    for i in range(nbr_players):
         list_of_fishers.append(Fisherman())
 
     fish_stock = FishStock()
     fish_stock.X = 10
 
-    for t in range(10):
-        fish_stock.fish_stock_change(list_of_fishers)
-        print(fish_stock.X)
+    effort_array = np.zeros([nbr_players, max_time])
+    harvest_array = np.zeros([nbr_players, max_time])
+    fish_stock_array = np.zeros([max_time])
 
+    # Calculate stable solution
+    stable_effort_all = stable_solution(list_of_fishers, fish_stock)
+    print("Stable effort: ", stable_effort_all)
+
+    for t in range(max_time):
+        fish_stock_array[t] = fish_stock.X
+
+        for i, fisher in enumerate(list_of_fishers):
+            effort_array[i, t] = fisher.effort
+            harvest_array[i, t] = fisher.harvest
+
+        fish_stock.fish_stock_change(list_of_fishers)
+
+    plt.plot(fish_stock_array)
+    plt.show()
 
 if __name__ == '__main__':
     main()
